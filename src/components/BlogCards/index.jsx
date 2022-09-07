11deb0ff2928125286cc559e34069ce7a86ff637
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-07 16:15:02 +0100

    dev

--- src/components/BlogCards/index.jsx
+++ src/components/BlogCards/index.jsx
@@ -4,10 +4,10 @@ function BlogCard(props) {
   return (
     <div className={'row m-0 blogcards'}> 
       {props?.blog?.map((blog, key) =>
-        <div key={key} className={  `${blog.isTop ? 'headeline-cards' : 'detailed-cards col-md-4'} my-3`}>
+        <div key={key} className={  `${blog.isTop ? 'headeline-cards' : 'detailed-cards col-md-4 mx-auto'} my-3`}>
            <div className="img">
               <img src={blog.img} alt="" /> 
-            </div>              
+          </div>     
               <div className="card-body">
               {blog.isTop && <div className='tag' >{blog.category}</div>}
               <h3>{blog.topic}</h3> 
