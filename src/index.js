cb22b5446f5274843182a2c8cd9f6443d6c2754e
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-09 16:15:14 +0100

    Added app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './router';
+import App from './App';
 import { store } from "./store";
 import { Provider } from "react-redux";
 import "./sass/index.scss"
