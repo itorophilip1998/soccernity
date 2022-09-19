dce65b60b74789bc469779a6c060badfa3be1e4e
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-19 05:05:24 +0100

    dev

--- src/pages/TermsAndPolicy/Policy.jsx
+++ src/pages/TermsAndPolicy/Policy.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 
 function Policy() {
-  return (
-    <div className="policy container">
-      
-    </div>
-  )
+    return (
+        <div className="policy container">
+            <h1>Privacy Policy for Soccernity
+            </h1>
+            <div className="welcome">
+                <p>At Soccernity, accessible from https://soccernity.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Soccernity and how we use it.
+                </p>
+                <p> If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
+                </p>
+                <p> This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Soccernity. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Free Privacy Policy Generator.
+                </p>
+            </div>
+            <div className="concent">
+                <h3>Consent</h3>
+                <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+
+                </p>
+            </div>
+            <div className="incollect">
+                <h3>Information we collect
+                </h3>
+                <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.
+                </p>
+                <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.
+                </p>
+                <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
+                </p>
+            </div>
+            <div className="info">
+                <h3>How we use your information
+                </h3>
+
+                <p>
+                    We use the information we collect in various ways, including to:
+                    <ul>
+                        <li>Provide, operate, and maintain our website</li>
+                        <li>Improve, personalize, and expand our website</li>
+                        <li>Understand and analyze how you use our website</li>
+                        <li>Develop new products, services, features, and functionality</li>
+                        <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+                        <li>Send you emails</li>
+                        <li>Find and prevent fraud</li>
+                    </ul>
+                </p>
+            </div>
+            <div className='logFiles'>
+                <h3>Log Files
+                </h3>
+                <p>
+                    Soccernity follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Policy
--- src/pages/TermsAndPolicy/Terms.jsx
+++ src/pages/TermsAndPolicy/Terms.jsx
@@ -125,6 +125,30 @@ const Terms = () => {
                 </p>
             </div>
 
+            <div className="removal">
+                <h3>Removal of links from our website
+                </h3>
+                <p>
+                    If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.
+
+                    We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+                </p>
+            </div>
+            <div className="Disclaimer">
+                <h3>Disclaimer</h3>
+                <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+
+                <ul>
+                    <li>limit or exclude our or your liability for death or personal injury;</li>
+                    <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+                    <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+                    <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+                    <li>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</li>
+
+                </ul>
+                <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+            </div>
+
         </div>
     )
 }
