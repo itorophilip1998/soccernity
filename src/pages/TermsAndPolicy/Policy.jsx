3f30b8df0470976a8026312ff746417bcf620008
itorophilip1998 <itorophilip1998@gmail.com>
2022-09-19 05:52:15 +0100

    termspolicy

--- src/pages/TermsAndPolicy/Policy.jsx
+++ src/pages/TermsAndPolicy/Policy.jsx
@@ -53,6 +53,105 @@ function Policy() {
                     Soccernity follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
                 </p>
             </div>
+            <div className="beacons">
+                <h3>Cookies and Web Beacons
+                </h3>
+                <p>
+                    Like any other website, Soccernity uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+                </p>
+            </div>
+            <div className="dart">
+                <h3>
+                    Google DoubleClick DART Cookie
+
+                </h3>
+                <p>
+                    Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads
+
+                </p>
+            </div>
+            <div className="adverp">
+                <h3>Our Advertising Partners
+                </h3>
+                <p>
+                    Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.
+                    <ul>
+                        <li>Google <br />
+                            <a href=' https://policies.google.com/technologies/ads' target="_blank">
+
+                                https://policies.google.com/technologies/ads</a>
+                        </li>
+                    </ul>
+
+                </p>
+            </div>
+            <div className="adverpP">
+                <h3>
+                    Advertising Partners Privacy Policies
+
+                </h3>
+                <p>
+                    ou may consult this list to find the Privacy Policy for each of the advertising partners of Soccernity.
+
+                    Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Soccernity, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+
+                    Note that Soccernity has no access to or control over these cookies that are used by third-party advertisers.
+                </p>
+            </div>
+            <div className="ccpa">
+                <h3>
+                    CCPA Privacy Rights (Do Not Sell My Personal Information)
+
+                </h3>
+                <p>
+                    Under the CCPA, among other rights, California consumers have the right to:
+
+                    Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+
+                    Request that a business delete any personal data about the consumer that a business has collected.
+
+                    Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+
+                    If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                </p>
+            </div>
+            <div className="GDPR">
+                <h3>
+                    GDPR Data Protection Rights
+
+                </h3>
+                <ul>
+                    <li>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+                    </li>
+                    <li>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
+                    </li>
+                    <li>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
+                    </li>
+                    <li>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
+                    </li>
+                    <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+                    </li>
+                    <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+                    </li>
+                    <li>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+                    </li>
+                    <li>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                    </li>
+                </ul>
+
+            </div>
+            <div className="childrenInfo">
+                <h3>
+                    Children's Information
+
+                </h3>
+                <p>
+                    Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+                </p>
+                <p>
+                    Soccernity does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+                </p>
+            </div>
         </div>
     )
 }
