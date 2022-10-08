41edb1a35f499a965cf7686538220dcf1854d621
itorophilip1998 <itorophilip1998@gmail.com>
2022-10-08 21:07:17 +0100

    working on leauges

--- src/pages/liveScore/components/Fixtures.jsx
+++ src/pages/liveScore/components/Fixtures.jsx
@@ -5,29 +5,41 @@ const Fixtures = () => {
      
 
     return (
-        <div className='fixtures'>
-            <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <div className='fixtures pl-md-3'>
+            <ul className="nav nav-pills mb-3 shadow" id="pills-tab" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Live</button>
+                    <button className="nav-link active" id="pills-live-tab" data-toggle="pill" data-target="#pills-live" type="button" role="tab" aria-controls="pills-live" aria-selected="true">{"Live"}</button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">{ }</button>
+                    <button className="nav-link" id="pills-today-tab" data-toggle="pill" data-target="#pills-today" type="button" role="tab" aria-controls="pills-today" aria-selected="false">{"Today"}</button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Tommorrow</button>
+                    <button className="nav-link" id="pills-day1-tab" data-toggle="pill" data-target="#pills-day1" type="button" role="tab" aria-controls="pills-day1" aria-selected="false">{moment().add(1, 'days').format("dddd")}</button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Tommorrow</button>
+                    <button className="nav-link" id="pills-day2-tab" data-toggle="pill" data-target="#pills-day2" type="button" role="tab" aria-controls="pills-day2" aria-selected="false">{moment().add(2, 'days').format("dddd")}</button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Tommorrow</button>
+                    <button className="nav-link" id="pills-day3-tab" data-toggle="pill" data-target="#pills-day3" type="button" role="tab" aria-controls="pills-day3" aria-selected="false">{moment().add(3, 'days').format("dddd")}</button>
                 </li>
             </ul>
 
             <div className="tab-content" id="pills-tabContent">
-                <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-                <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-                <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                <div className="tab-pane fade show active" id="pills-live" role="tabpanel" aria-labelledby="pills-live-tab">
+                    Live
+                </div>
+                <div className="tab-pane fade" id="pills-today" role="tabpanel" aria-labelledby="pills-today-tab">
+                    Today
+                </div>
+                <div className="tab-pane fade" id="pills-day1" role="tabpanel" aria-labelledby="pills-day1-tab">
+                    day1
+                </div>
+                <div className="tab-pane fade" id="pills-day2" role="tabpanel" aria-labelledby="pills-day2-tab">
+                    day2
+                </div>
+                <div className="tab-pane fade" id="pills-day3" role="tabpanel" aria-labelledby="pills-day3-tab">
+                    day3
+                </div>
             </div>
         </div>
     )
