dafe19d385394a44a32b5f196028ad4e07b1dbda
sph <itorophilip1998@gmail.com>
2022-10-17 21:11:33 +0100

    dev

--- src/pages/liveScore/components/MatchDetails/Match/MatchSummary.jsx
+++ src/pages/liveScore/components/MatchDetails/Match/MatchSummary.jsx
@@ -8,26 +8,26 @@ function MatchSummary() {
     const id = query.get("id");
     const array = query.get("array");
     const dispatch = useDispatch()
-    // const match_summary = useSelector((state) => state.livescores.match_summary);
+    const match_summary = useSelector((state) => state.livescores.match_summary);
     const away_id = window.localStorage.getItem("away")
     useEffect(() => {
         dispatch(getEvents(id))
     }, [])
     const scores = useSelector((state) => state.livescores?.[array]);
-    const match_summary = scores?.find((data) => data?.fixture?.id === parseInt(id))
-    const firsthalf = moment(match_summary?.fixture?.periods?.first).format("mm")
-    const secondhalf = moment(match_summary?.fixture?.periods?.second).format("mm")
-    console.log(match_summary)
-    // const timeEllapse = (time) => {
-    //     if (time < 45) {
-    //         time = "FIRST HALF"
-    //     }
-    //     else if (time > 45) {
-    //         time = "SECOND HALF"
-    //     }
+    const summarry = scores?.find((data) => data?.fixture?.id === parseInt(id))
+    const firsthalf = moment(summarry?.fixture?.periods?.first).format("mm")
+    const secondhalf = moment(summarry?.fixture?.periods?.second).format("mm")
+    // console.log(match_summary)
+    // // const timeEllapse = (time) => {
+    // //     if (time < 45) {
+    // //         time = "FIRST HALF"
+    // //     }
+    // //     else if (time > 45) {
+    // //         time = "SECOND HALF"
+    // //     }
 
-    //     return time;
-    // }
+    // //     return time;
+    // // }
 
 
     return (
@@ -38,7 +38,7 @@ function MatchSummary() {
                     First Half <span className="goals">{match_summary?.score?.halftime?.home ?? 0}  -   {match_summary?.score?.halftime?.away ?? 0}</span>
                 </div>
                 {
-                    match_summary?.events?.map((item, key) =>
+                    match_summary?.map((item, key) =>
                         <div className={`event ${item?.time?.elapsed > parseInt(firsthalf) && 'd-none'}`} key={key}  >
                             {item?.team?.id !== parseInt(away_id) && <div className="home  ">
                                 <span className="time ">{item?.time?.elapsed}’ {item?.time?.extra ? ` + ${item?.time?.extra}` : ""}</span>
@@ -69,7 +69,7 @@ function MatchSummary() {
                     Second Half <span className="goals">{match_summary?.score?.fulltime?.home ?? 0} - {match_summary?.score?.fulltime?.away ?? 0}</span>
                 </div>
                 {
-                    match_summary?.events?.map((item, key) =>
+                    match_summary?.map((item, key) =>
                         <div className={`event ${item?.time?.elapsed < parseInt(secondhalf) && 'd-none'}`} key={key}  >
 
                             {item?.team?.id !== parseInt(away_id) && <div className="home  ">
