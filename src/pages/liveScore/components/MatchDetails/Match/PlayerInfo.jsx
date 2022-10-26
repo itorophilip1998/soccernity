11448e04b57e95c1ad3859261f6dba2fa622c507
itorophilip1998 <itorophilip1998@gmail.com>
2022-10-26 08:54:25 +0100

    de

--- src/pages/liveScore/components/MatchDetails/Match/PlayerInfo.jsx
+++ src/pages/liveScore/components/MatchDetails/Match/PlayerInfo.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getCoach } from '../../../../../store/LiveScores/Matches';
-
+/*eslint-disabl*/
 const PlayerInfo = ({ item }) => {
     const dispatch = useDispatch()
     const player = useSelector((state) => state.livescores.player);
