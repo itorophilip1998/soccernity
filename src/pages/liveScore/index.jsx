b4198c3a2543245603d12072cc9cb44402c3a134
sph <itorophilip1998@gmail.com>
2022-11-07 23:52:14 +0100

    dev

--- src/pages/liveScore/index.jsx
+++ src/pages/liveScore/index.jsx
@@ -14,7 +14,7 @@ const LiveScore = () => {
                 </div>
             </div> */}
 
-            <div id="wg-api-football-fixtures"
+            {/* <div id="wg-api-football-fixtures"
                 data-host="v3.football.api-sports.io"
                 data-refresh="60"
                 data-date="2022-02-11"
@@ -22,6 +22,25 @@ const LiveScore = () => {
                 data-theme=""
                 data-show-errors="false"
                 class="api_football_loader">
+            </div>
+            
+            
+            */}
+
+            <div id="wg-api-football-games"
+                data-host="v3.football.api-sports.io"
+                data-key="da28481e728a0398152f9e642f2b6d8e"
+                data-date=""
+                data-league=""
+                data-season=""
+                data-theme=""
+                data-refresh="15"
+                data-show-toolbar="true"
+                data-show-errors="false"
+                data-show-logos="true"
+                data-modal-game="true"
+                data-modal-standings="true"
+                data-modal-show-logos="true">
             </div>
         </div>
     )
