1d07ea3f60761ecc0c421539060228705a4ef8fe
itorophilip1998 <itorophilip1998@gmail.com>
2022-11-15 00:40:34 +0100

    dev

--- src/pages/Community/components/Post/Postcards.jsx
+++ src/pages/Community/components/Post/Postcards.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 function Postcards() {
   return (
-    <div>
-      sjhdjshdjshjd
+    <div className='post-cards'>
+      <div className="row m-0">
+        <div className="col-2">
+          <img src="/images/emeka.png" alt="" />
+        </div>
+        <div className="col-10">
+          <p>
+            <div className="users">
+              <span className="name">Emeka John</span>
+              <span className="username">@mekusa</span>
+              <span className="time">2m</span>
+            </div>
+            <div className="img-Box">
+              <img src="/images/box/vr.png" alt="" />
+            </div>
+          </p>
+          <p className="bio">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+          </p>
+          <div className="img-container">
+            <img src="/images/postImg.png" alt="" />
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
