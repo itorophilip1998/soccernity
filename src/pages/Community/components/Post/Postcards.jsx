86c13a9f24b4e9b1bf5b9eb8fcb3271cc91d4156
itorophilip1998 <itorophilip1998@gmail.com>
2022-11-15 12:09:50 +0100

    dev

--- src/pages/Community/components/Post/Postcards.jsx
+++ src/pages/Community/components/Post/Postcards.jsx
@@ -1,51 +1,60 @@
 import React from 'react'
-
+const data = [1, 2, 3, 4, 5, 7, 6];
 function Postcards() {
-  return (
-    <div className='post-cards '>
-      <div className="row m-0">
-        <div className="col-2">
-          <img src="/images/emeka.png" alt="" />
-        </div>
-        <div className="col-10">
-          <p>
-            <div className="users">
-              <span className="name">Emeka John</span>
-              <span className="username">@mekusa</span>
-              <span className="time">2m</span>
-            </div>
-            <div className="img-Box ">
-              <img src="/images/box/vr.png"  alt="" />
-            </div>
-          </p>
-          <p className="bio">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
-          </p>
-          <div className="img-container">
-            <img src="/images/postImg.png" alt="" />
 
+  return (
+    <>
+      {
+        data.map((item, key) => (
+          <div className='post-cards' key={key}>
+            <div className="row m-0">
+              <div className="col-2">
+                <img src="/images/emeka.png" alt="" />
+              </div>
+              <div className="col-10 p-0 ">
+                <p className='topinfo'>
+                  <div className="users">
+                    <span className="name">Emeka John</span>
+                    <span className
+                      ="username">@mekusa</span>
+                    <span className="time">2m</span>
+                  </div>
+                  <div className="imgBox ">
+                    <img src="/images/box/vr.png" className='link' alt="" />
+                  </div>
+                </p>
+                <p className="post_text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+                </p>
+                {(key === 1 || key === 4) && <div className="img-container">
+                  <img src="/images/postImg.png" className='w-100' alt="" />
+
+                </div>}
+                <div className="footer-box">
+                  <span className="tag">
+                    <img src="/images/box/comment.png" alt="" />
+                    <span>25</span>
+                  </span>
+                  <span className="tag">
+                    <img src="/images/box/love.png" alt="" />
+                    <span >124</span>
+                  </span>
+                  <span className="tag">
+                    <img src="/images/box/share.png" alt="" />
+                    <span >345</span>
+                  </span>
+                  <span className="tag">
+                    <img src="/images/box/connect.png" alt="" />
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="footer-box">
-            <span>
-              <img src="/images/box/comment.png" alt="" />
-              <span className="tag">25</span>
-          </span>
-            <span>
-              <img src="/images/box/love.png" alt="" />
-              <span className="tag">124</span>
-          </span>
-            <span>
-              <img src="/images/box/share.png" alt="" />
-              <span className="tag">345</span>
-          </span>
-            <span>
-              <img src="/images/box/connect.png" alt="" /> 
-          </span>
-          </div>
-        </div>
-      </div>
-    </div>
+        ))
+      }
+    </>
   )
 }
+
 
 export default Postcards
