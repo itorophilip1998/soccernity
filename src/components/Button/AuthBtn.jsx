c7d0592ab2a13f73a39a66115893ce2cea730f9a
Itorophilip1998 <itorophilip1998@gmail.com>
2022-12-17 18:29:59 +0100

    dev

--- src/components/Button/AuthBtn.jsx
+++ src/components/Button/AuthBtn.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const AuthBtn = ({ name }) => {
-    console.log(name)
     return (
         <div className="form-group authButton">
             <button className="btn form-control" type='submit'>{name}</button>
--- src/pages/Community/components/Profile/ViewProfile.jsx
+++ src/pages/Community/components/Profile/ViewProfile.jsx
@@ -4,10 +4,52 @@ function ViewProfile() {
   return (
     <div className='viewProfile'>
       <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt=""/>
+        <img class="card-img-top" src="/images/profile/defaultbg.png" alt="" />
         <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <p class="card-text">Text</p>
+          <img src="/images/profile/profile.png" className='avatar' alt="" />
+          <div className="row m-0 ">
+            <div className="col-md-12 p-0 text-right firstCol">
+              <div className="isGuest">
+                {/* dropdown */}
+                <div class="dropdown">
+                  <div class="dropdown-menu shadowBox" aria-labelledby="triggerisguest">
+                    <button class="dropdown-item"  > <img src="/images/profile/clip.png" alt="" /> Copy Profile Link</button>
+                    <button class="dropdown-item"  > <img src="/images/profile/share.png" alt="" /> Share Profile Via</button>
+                    <button class="dropdown-item"  > <img src="/images/profile/block.png" alt="" /> Block <span className='font-weight-normal'>nelson_iheagwam</span></button>
+                  </div>
+                </div>
+                <button className="btn" id="triggerisguest" data-toggle="dropdown" aria-haspopup="true"><img src="/images/profile/vh.png" alt="" /></button>
+                <button className="btn"><img src="/images/profile/msg.png" alt="" /></button>
+                <button className="btn">Follow</button>
+              </div>
+              <div className="isAuth d-none">
+                <button className="btn">Edit Profile</button>
+              </div>
+            </div>
+
+
+            <div className="col-md-12 text-box">
+              <h4 class="fullname">Adeniyi Christiana
+                <span className="isclub"><img src="/images/profile/isclub.png" alt="" /></span>
+                <span className="location"><img src="/images/profile/location.png" alt="" /> Port Harcourt</span>
+              </h4>
+              <p class="username">@nelson_iheagwam</p>
+              <p className="bio">Fine Girl, Hard Arsenal Stan!</p>
+              <p className="follows">
+                <span className="info ml-0">
+                  5,500
+                  <span className="details">Followers</span>
+                </span>
+                <div className="dotDivider"></div>
+                <span className="info">
+                  5,500
+                  <span className="details">Following</span>
+                </span>
+              </p>
+            </div>
+
+
+          </div>
         </div>
       </div>
     </div>
